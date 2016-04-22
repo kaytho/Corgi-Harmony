02f2d7487d9e98c29498412a444c95c38f19ffc8
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-04-22 16:20:41 -0500

    routes complete

--- routes/corgis.js
+++ routes/corgis.js
@@ -3,10 +3,24 @@ var router = express.Router();
 var mongoose = require('mongoose')
 var Corgi = require('../models/corgi')
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Corgi-Harmony' });
 });
+
+
+router.get('/corgi', function(req, res, next) {
+  Corgi.find({like: null}, function(err, corgis) {
+    if (err) {
+      console.log("error");
+      } else {
+        var randomNumber = Math.floor(Math.random() * corgis.length);
+        var corgi = corgis[randomNumber]
+        res.json(corgi);
+      }
+    }
+  });
 
 
 router.put('/:id', function(req, res, next) {
@@ -16,5 +30,6 @@ router.put('/:id', function(req, res, next) {
     res.json(corgi);
   });
 });
+
 
 module.exports = router;
